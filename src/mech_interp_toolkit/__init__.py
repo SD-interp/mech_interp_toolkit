1d9db90c6809d76d15545b68fa3cfac124bbfce0
Shantanu Darveshi <shantanu.darveshi@gmail.com>
2026-01-13 15:52:27 +0530

    Revert version number to 0.0.0 in __init__.py

--- src/mech_interp_toolkit/__init__.py
+++ src/mech_interp_toolkit/__init__.py
@@ -4,7 +4,7 @@ Mech-Interp-Toolkit: A Comprehensive Library for Mechanistic Interpretability
 This library provides a suite of tools for researchers and developers to dissect and understand the internal workings of large language models (LLMs).
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.0"
 
 from . import tokenizer
 from . import utils
